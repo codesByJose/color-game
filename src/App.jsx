33d72a0f07fc667bad991e6bd21b9474fb2acf46
Joest Thomas <joesthomas2003@gmail.com>
2025-02-07 08:21:47 +0900

    nex commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {  Box, Button, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import {  Box, Button, Center, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 
 
 const colorOfButtons = ["#0000FF", "#FF0000", "#00FF00", "#FFC0CB", "#FFA500", "#A52A2A"];
@@ -12,6 +12,7 @@ export default function App() {
   const [message, setMessage] = useState("");
   const [moves, setMoves] = useState([]);
   const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false)
   const [allScores, setAllScores] = useState([]);
 
   function getRandomColor(index) {
@@ -30,11 +31,13 @@ export default function App() {
       setMessage("Correct Guess");
       setScore(score + 1);
       setButtonColor(Array(colorOfButtons.length).fill(color));
+      
+      setGameOver(true)
       setTimeout(() => {
         reset();
-      }, 2000);
-      setMoves([]);
-    }}
+      }, 1000);
+    }
+  }       
     
 
   function reset() {
@@ -44,6 +47,7 @@ export default function App() {
     setTargetColor(target[Math.floor(Math.random() * target.length)]);
     setButtonColor(Array(colorOfButtons.length).fill(defaultColor));
     setClickedButtons(Array(colorOfButtons.length).fill(false));
+    setMessage("");
     setMoves([]);
     
   }
@@ -51,14 +55,15 @@ export default function App() {
   return (
     <Box display={"flex"} margin={3} flexDir={{base : "column", sm : "column", md : "row"}} p={5} justifyContent={"space-between"} alignItems={"center"} w={"800px"} boxShadow= {" 0 0 10px 0 rgba(0, 0, 0, 0.5);"} >
       <Box flexBasis={"70%"}>
-      <Text data-testid="gameInstruction" m={4} fontSize={"2em"} fontWeight={900}>Guess the correct color button</Text>
+      <Text data-testid="gameInstructions" m={4} fontSize={"2em"} fontWeight={900} >Guess the correct color button</Text>
+      <Text data-testid="gameStatus" fontSize={"1.3em"} mb={3} textAlign={"center"} bgColor={"orange.900"} color={"whiteAlpha.700"} p={2} borderRadius={3} >Message: {message}</Text>
         <Box textAlign={"center"} >
-        <Button data-testid="targetColor"  backgroundColor={targetColor} _hover={{background: targetColor}}  width={ {base : "40", sm : "150", md : "150"}} height = {{base : "40", sm : "150", md : "150"}} marginBottom = {7} > </Button>
+        <Button data-testid="colorBox"  backgroundColor={targetColor} _hover={{background: targetColor}}  width={ {base : "40", sm : "150", md : "150"}} height = {{base : "120", sm : "150", md : "150"}} marginBottom = {7} > </Button>
         </Box>
-        <Box display={"grid"} gridTemplateColumns={{base : "repeat(3, 3fr)", sm : "repeat(2, 3fr)"}} gap={4}>
+        <Box display={"grid"} gridTemplateColumns={{base : "repeat(3, 3fr)", sm : "repeat(3, 3fr)", md : "repeat(3, 3fr)"}} gap={2}>
           {buttonColor.map((color, index) => (
-            <Button data-testid="buttonColors"
-            backgroundColor={color}  width={ {base : "20", sm : "150", md : "150"}} height = {{base : "20", sm : "150", md : "150"}} _hover= {{backgroundColor: color}}
+            <Button data-testid="colorOption"
+            backgroundColor={color}  w={ {base : "20", sm : "40", md : "40"}} h ={{base : "20", sm : "140", md : "150"}} _hover= {{backgroundColor: color}}
               key={index}
               onClick={() => getRandomColor(index)}
               disabled={clickedButtons[index]}
@@ -69,22 +74,22 @@ export default function App() {
       </Box>
       
       <Box flexBasis={"30%"} display={"flex"} flexDir={"column"} justifyContent={"center"}  p={4}>
-      <Text data-testid="message" fontSize={"1em"} mb={3}>Message: {message}</Text>
+      
       <Box  w={"100%"}>
-        <Text data-testid="moves" fontWeight={600} font>Moves</Text>
+        <Text data-testid="moves" fontWeight={600} font textAlign={"center"} fontSize={"1.2em"} mb={4}>Moves</Text>
         <UnorderedList >
           {moves.map((m, i) => (
-            <ListItem listStyleType={"none"} key={i}># {m}</ListItem>
+            <ListItem listStyleType={"none"} key={i} mb={3}># {m}</ListItem>
           ))}
         </UnorderedList>
       </Box>
       <Box display={"flex"} justifyContent={"center"}>
         <Text pr={4}>Score</Text>
-        <Text data-testid="gameStatus">{score}</Text>
+        <Text data-testid="score">{score}</Text>
       </Box>
      
       
-      <Button data-testid="newGameButton" onClick={reset}>Reset</Button>
+      <Button data-testid="newGameButton" onClick={reset} mt={2}>Reset</Button>
       </Box>
     </Box>
   );
